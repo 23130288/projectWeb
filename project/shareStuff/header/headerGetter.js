50456f9cd119cb313af6ddc1d63dfa22a5fa0fac
23130288 <23130288@st.hcmuaf.edu.vn>
2025-11-24 23:33:53 +0700

    adding header and footer to my my pages
    also a javascript to help importing header and footer easier so please read

---
+++ project/shareStuff/header/headerGetter.js
@@ -0,0 +1,18 @@
+fetch('../shareStuff/header/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    });
+
+/*
+================= HOW TO USE =================
+Bỏ vào file ở đây như vậy, còn hạn chế nên đừng có tạo bỏ file linh tinh
+chỉ hoạt động với file theo format này
+trang_chu
+  + trang_chu.html
+
+<header>
+    <div id="header-placeholder"></div>
+    <script src="../shareStuff/header/headerGetter.js"></script>
+</header>
+*/
