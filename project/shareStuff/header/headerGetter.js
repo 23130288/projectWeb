0b4c47cb048d80e67392212b949012244484f718
Trương Hoàng Minh Khôi <23130159@st.hcmuaf.edu.vn>
2025-11-26 18:11:46 +0700

    gắn link header,footer cho trang tìm kiếm ,trang chủ,trang sản phẩm nổi bật

---
+++ project/shareStuff/header/headerGetter.js
@@ -0,0 +1,5 @@
+fetch('../shareStuff/header/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    });
