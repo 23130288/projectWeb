1a7074733f0b92e48f72a62530c70d0b7c3594d2
23130288 <23130288@st.hcmuaf.edu.vn>
2025-12-01 22:12:58 +0700

    are we done yet
    fixing some dumb stuff in cartPage, dangKy, dangNhap and adding suggestion box for header

--- project/shareStuff/header/headerGetter.js
+++ project/shareStuff/header/headerGetter.js
@@ -2,6 +2,11 @@ fetch('../shareStuff/header/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
+
+        // TẢI file JS của header
+        const script = document.createElement('script');
+        script.src = '../shareStuff/header/header.js';
+        document.body.appendChild(script);
     });
 
 /*
