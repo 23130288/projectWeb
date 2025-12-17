2ba23e477f431f598febd60be9d33b0028033fb7
Nguyễn Thanh Hải <23130086@st.hcmuaf.edu.vn>
2025-12-17 23:38:57 +0700

    tạo userDao, user
    dùng dữ liệu tạo từ userdao thay vì code cứng. sửa lại giao diện hiện thông báo lỗi sai email hoặc pass

--- src/main/java/org/example/projectweb/DangNhapController.java
+++ src/main/java/org/example/projectweb/DangNhapController.java
@@ -5,7 +5,8 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import org.example.projectweb.dao.userDao;
+import org.example.projectweb.model.User;
 
 @WebServlet(name = "DangNhapController", value = "/dang_nhap")
 public class DangNhapController extends HttpServlet {
@@ -15,15 +16,24 @@ public class DangNhapController extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         String email = request.getParameter("email");
         String pass = request.getParameter("password");
 
-        // GIẢ LẬP xác thực (sau này thay bằng DB)
-        if ("user@gmail.com".equals(email) && "123".equals(pass)) {
+        userDao dao = new userDao();
+        User user = dao.login(email, pass);
+
+        if (user != null) {
+            // đăng nhập thành công
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+
             response.sendRedirect("tai_khoan");
         } else {
-            request.setAttribute("err", "bạn nhập sai email hoặc mật khẩu");
+            // đăng nhập thất bại
+            request.setAttribute("err", "Bạn nhập sai email hoặc mật khẩu");
             request.getRequestDispatcher("dang_nhap/dang_nhap.jsp").forward(request, response);
         }
     }
---
+++ src/main/java/org/example/projectweb/dao/userDao.java
@@ -0,0 +1,36 @@
+package org.example.projectweb.dao;
+
+import org.example.projectweb.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class userDao {
+    static Map<Integer, User> data = new HashMap<>();
+
+    static {
+        data.put(1, new User(1, "nguyen van a", "nguyenvana@gmail.com", "123", "duong abc", "12/11/2010", "nguyenvana.jpg", "user", "09444423670", true));
+        data.put(2, new User(1, "nguyen van b", "nguyenvanb@gmail.com", "123", "duong abc", "15/5/2009", "nguyenvanb.jpg", "user", "09638256192", true));
+        data.put(3, new User(1, "nguyen van c", "nguyenvanc@gmail.com", "123", "duong abc", "24/1/2008", "nguyenvanc.jpg", "user", "09628237362", true));
+        data.put(4, new User(1, "tran van a", "tranvana@gmail.com", "123", "duong abc", "2/7/2004", "tranvana.jpg", "user", "092129352761", true));
+    }
+
+    public User login(String email, String password) {
+        for (User u : data.values()) {
+            if (u.getEmail().equals(email) && u.getPassword().equals(password)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public List<User> getListUserId() {
+        return new ArrayList<>(data.values());
+    }
+
+    public User getUserById(int userId) {
+        return data.getOrDefault(userId, null);
+    }
+}
